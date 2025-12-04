2e228725b05536c58788daf1b12cc12910f28838
FredrickBarrett <f.barrett12@gmail.com>
2025-12-04 00:28:45 -0600

    cors and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :tickets, only: [:index, :show, :create, :update] do
+        member do
+          post :link_known_issues
+        end
+      end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+      resources :known_issues, only: [:index, :show, :create, :update]
+      resources :glossary_terms, only: [:index, :show, :create, :update]
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/health", to: proc { [200, {}, ['{"status":"ok"}']] }
 end
